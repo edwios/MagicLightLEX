9e3c010c469cb9e8641bf410618e11ad1cfc6170
edwios <edwios42@gmail.com>
2016-12-31 15:21:45 +0800

    Initial Commit

---
+++ MagicLightLEX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MagicLightLEX
+//
+//  Created by Edwin Tam on 31/12/2016.
+//  Copyright © 2016 ioStation Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
